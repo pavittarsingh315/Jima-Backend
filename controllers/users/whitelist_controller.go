876d87e0f2155530130385a389ee0b2faf386945
pavittarsingh315 <pavittarsingh12364@gmail.com>
2022-06-27 22:51:23 -0700

    Implemented add/remove methods to whitelist.

--- controllers/users/whitelist_controller.go
+++ controllers/users/whitelist_controller.go
@@ -1,20 +1,89 @@
 package users
 
 import (
+	"NeraJima/configs"
+	"NeraJima/models"
 	"NeraJima/responses"
-	"fmt"
+	"context"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func AddUserToWhitelist(c *fiber.Ctx) error {
-	profileId := c.Params("profileId")
-	return c.Status(fiber.StatusOK).JSON(responses.SuccessResponse{Status: fiber.StatusOK, Message: "Success", Data: &fiber.Map{"data": fmt.Sprintf("Whitelisted %s", profileId)}})
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+	var whitelistObj models.Whitelist
+	var toBeAddedProfile models.Profile
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	profileId, _ := primitive.ObjectIDFromHex(c.Params("profileId"))
+
+	if reqProfile.Id == profileId {
+		return c.Status(fiber.StatusBadRequest).JSON(responses.ErrorResponse{Status: fiber.StatusBadRequest, Message: "Error", Data: &fiber.Map{"data": "Cannot whitelist yourself."}})
+	}
+
+	err := configs.WhitelistCollection.FindOne(ctx, bson.M{"ownerId": reqProfile.Id, "allowedId": profileId}).Decode(&whitelistObj)
+	if err == nil { // no error => user is already whitelisted
+		return c.Status(fiber.StatusBadRequest).JSON(responses.ErrorResponse{Status: fiber.StatusBadRequest, Message: "Error", Data: &fiber.Map{"data": "User is already whitelisted."}})
+	}
+
+	err = configs.ProfileCollection.FindOne(ctx, bson.M{"_id": profileId}).Decode(&toBeAddedProfile)
+	if err != nil { // error => user doesn't exist
+		return c.Status(fiber.StatusBadRequest).JSON(responses.ErrorResponse{Status: fiber.StatusBadRequest, Message: "Error", Data: &fiber.Map{"data": "Could not whitelist user."}})
+	}
+
+	newWhitelistObj := models.Whitelist{
+		Id:        primitive.NewObjectID(),
+		OwnerId:   reqProfile.Id,
+		AllowedId: profileId,
+	}
+
+	_, err = configs.WhitelistCollection.InsertOne(ctx, newWhitelistObj)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.ErrorResponse{Status: fiber.StatusInternalServerError, Message: "Error", Data: &fiber.Map{"data": "Error. Please try again."}})
+	}
+
+	update := bson.M{"numWhitelisted": reqProfile.NumWhitelisted + 1, "lastUpdate": time.Now()}
+	_, err = configs.ProfileCollection.UpdateOne(ctx, bson.M{"userId": reqProfile.UserId}, bson.M{"$set": update})
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.ErrorResponse{Status: fiber.StatusInternalServerError, Message: "Error", Data: &fiber.Map{"data": "Unexpected error..."}})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.SuccessResponse{Status: fiber.StatusOK, Message: "Success", Data: &fiber.Map{"data": "User whitelisted."}})
 }
 
 func RemoveUserFromWhitelist(c *fiber.Ctx) error {
-	profileId := c.Params("profileId")
-	return c.Status(fiber.StatusOK).JSON(responses.SuccessResponse{Status: fiber.StatusOK, Message: "Success", Data: &fiber.Map{"data": fmt.Sprintf("Blacklisted %s", profileId)}})
+	var reqProfile models.Profile = c.Locals("profile").(models.Profile)
+	var whitelistObj models.Whitelist
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	profileId, _ := primitive.ObjectIDFromHex(c.Params("profileId"))
+
+	if reqProfile.Id == profileId {
+		return c.Status(fiber.StatusBadRequest).JSON(responses.ErrorResponse{Status: fiber.StatusBadRequest, Message: "Error", Data: &fiber.Map{"data": "Cannot blacklist yourself."}})
+	}
+
+	err := configs.WhitelistCollection.FindOne(ctx, bson.M{"ownerId": reqProfile.Id, "allowedId": profileId}).Decode(&whitelistObj)
+	if err != nil { // error => user is not whitelisted
+		return c.Status(fiber.StatusBadRequest).JSON(responses.ErrorResponse{Status: fiber.StatusBadRequest, Message: "Error", Data: &fiber.Map{"data": "User is already blacklisted."}})
+	}
+
+	_, err = configs.WhitelistCollection.DeleteOne(ctx, bson.M{"ownerId": reqProfile.Id, "allowedId": profileId})
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.ErrorResponse{Status: fiber.StatusInternalServerError, Message: "Error", Data: &fiber.Map{"data": "Unexpected error..."}})
+	}
+
+	update := bson.M{"numWhitelisted": reqProfile.NumWhitelisted - 1, "lastUpdate": time.Now()}
+	_, err = configs.ProfileCollection.UpdateOne(ctx, bson.M{"userId": reqProfile.UserId}, bson.M{"$set": update})
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(responses.ErrorResponse{Status: fiber.StatusInternalServerError, Message: "Error", Data: &fiber.Map{"data": "Unexpected error..."}})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(responses.SuccessResponse{Status: fiber.StatusOK, Message: "Success", Data: &fiber.Map{"data": "User blacklisted."}})
 }
 
 func GetWhitelist(c *fiber.Ctx) error {
